eb0c325664a8f0c9183b35709cbb9c06b1df4f1e
youngyif <476391708@qq.com>
2022-10-17 16:38:15 +0800

    1

--- command/eval.sh
+++ command/eval.sh
@@ -3,7 +3,7 @@
 
 cd /mnt/cephfs/home/yangyifan/yangyifan/code/learnToSyLf/Ha-NeRF; source activate ;conda activate HaNeRF;
 nerf_out_dim1=64
-exp_name1="hanerf_encoder"
+exp_name1="debug"
 # model_mode1="1-1" #|1-4-1|1-1
 # decoder='stylenerf'  #esrgan||stylenerf
 ckpt_path1="/mnt/cephfs/dataset/NVS/nerfInWild/experimental_results/ckpts/${exp_name1}/last.ckpt"
@@ -12,7 +12,7 @@ dataset_name1='phototourism'
 save_dir1=/mnt/cephfs/dataset/NVS/nerfInWild/experimental_results
 # decoder_num_res_blocks=1
 
-CUDA_VISIBLE_DEVICES=0,1 python train_mask_grid_sample.py   --root_dir $root_dir1 --dataset_name $dataset_name1   --save_dir $save_dir1 --img_downscale 2 --use_cache   --N_importance 64 --N_samples 64   --num_epochs 20 --batch_size 1024   --optimizer adam --lr 5e-4 --lr_scheduler cosine   --exp_name $exp_name1 --N_emb_xyz 15 --N_vocab 1500   --maskrs_max 5e-2 --maskrs_min 6e-3 --maskrs_k 1e-3 --maskrd 0   --N_a 48 --weightKL 1e-5 --weightRecA 1e-3 --weightMS 1e-6   --num_gpus 2 --chunk 131072   --encode_a  #--model_mode $model_mode1 --decoder $decoder --decoder_num_res_blocks $decoder_num_res_blocks --num_epochs 1
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train_mask_grid_sample.py   --root_dir $root_dir1 --dataset_name $dataset_name1   --save_dir $save_dir1 --img_downscale 2 --use_cache   --N_importance 64 --N_samples 64   --num_epochs 20 --batch_size 1024   --optimizer adam --lr 5e-4 --lr_scheduler cosine   --exp_name $exp_name1 --N_emb_xyz 15 --N_vocab 1500   --maskrs_max 5e-2 --maskrs_min 6e-3 --maskrs_k 1e-3 --maskrd 0   --N_a 48 --weightKL 1e-5 --weightRecA 1e-3 --weightMS 1e-6   --num_gpus 4 --chunk 131072   --encode_a  #--model_mode $model_mode1 --decoder $decoder --decoder_num_res_blocks $decoder_num_res_blocks --num_epochs 1
 
 cd /mnt/cephfs/dataset/NVS/nerfInWild/experimental_results/logs/$exp_name1/codes
 
