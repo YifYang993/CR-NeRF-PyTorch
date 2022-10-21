2b93a88168bf785908c55a7ff621ed9730922b3f
youngyif <476391708@qq.com>
2022-10-21 20:43:26 +0800

    more ablations

--- command/eval.sh
+++ command/eval.sh
@@ -3,7 +3,7 @@
 
 cd /mnt/cephfs/home/yangyifan/yangyifan/code/learnToSyLf/Ha-NeRF; source activate ;conda activate HaNeRF;
 nerf_out_dim1=64
-exp_name1="hanerf_encode_random_mask"
+exp_name1="hanerf_noencode_norandom_nomask"
 # exp_name1="debug"
 # model_mode1="1-1" #|1-4-1|1-1
 # decoder='stylenerf'  #esrgan||stylenerf
@@ -14,7 +14,7 @@ save_dir1=/mnt/cephfs/dataset/NVS/nerfInWild/experimental_results
 img_downscale=4
 # decoder_num_res_blocks=1
 
-CUDA_VISIBLE_DEVICES=1,2,3,7 python train_mask_grid_sample.py   --root_dir $root_dir1 --dataset_name $dataset_name1   --save_dir $save_dir1 --img_downscale $img_downscale --use_cache   --N_importance 64 --N_samples 64   --num_epochs 20 --batch_size 1024   --optimizer adam --lr 5e-4 --lr_scheduler cosine   --exp_name $exp_name1 --N_emb_xyz 15 --N_vocab 1500   --use_mask  --maskrs_max 5e-2 --maskrs_min 6e-3 --maskrs_k 1e-3 --maskrd 0   --N_a 48 --weightKL 1e-5 --weightRecA 1e-3 --weightMS 1e-6   --num_gpus 4 --chunk 131072  --encode_a --encode_random  #--model_mode $model_mode1 --decoder $decoder --decoder_num_res_blocks $decoder_num_res_blocks --num_epochs 1
+CUDA_VISIBLE_DEVICES=4,5 python train_mask_grid_sample.py   --root_dir $root_dir1 --dataset_name $dataset_name1   --save_dir $save_dir1 --img_downscale $img_downscale --use_cache   --N_importance 64 --N_samples 64   --num_epochs 20 --batch_size 1024   --optimizer adam --lr 5e-4 --lr_scheduler cosine   --exp_name $exp_name1 --N_emb_xyz 15 --N_vocab 1500     --maskrs_max 5e-2 --maskrs_min 6e-3 --maskrs_k 1e-3 --maskrd 0   --N_a 48 --weightKL 1e-5 --weightRecA 1e-3 --weightMS 1e-6   --num_gpus 2 --chunk 131072  #--use_mask #--encode_a --encode_random  #--model_mode $model_mode1 --decoder $decoder --decoder_num_res_blocks $decoder_num_res_blocks --num_epochs 1
 
 cd /mnt/cephfs/dataset/NVS/nerfInWild/experimental_results/logs/$exp_name1/codes
 
@@ -26,11 +26,11 @@ CUDA_VISIBLE_DEVICES=4 python eval.py \
   --N_samples 256 --N_importance 256 --N_emb_xyz 15 \
   --N_vocab 1500  \
   --ckpt_path $ckpt_path1 \
-  --chunk 2048 --img_wh 320 240   --encode_a #--model_mode $model_mode1 --decoder $decoder --decoder_num_res_blocks $decoder_num_res_blocks
+  --chunk 2048 --img_wh 320 240   #--encode_a #--model_mode $model_mode1 --decoder $decoder --decoder_num_res_blocks $decoder_num_res_blocks
 
 CUDA_VISIBLE_DEVICES=4  python eval_metric.py \
   --root_dir $root_dir1 \
   --save_dir $save_dir1 \
   --dataset_name $dataset_name1 --scene_name $exp_name1 \
-  --split test_test --img_downscale 2 \
+  --split test_test --img_downscale $img_downscale \
   --img_wh 320 240
--- command/eval_only_orgnerf.sh
+++ command/eval_only_orgnerf.sh
@@ -8,7 +8,7 @@ nerf_out_dim1=64
 # exp_name1="gnerf_fvr_baseline_no_perturb64_updowndecoder1_sigmoid"
 # exp_name1="gnerf_fvr_baseline_no_perturb64_updowndecoder1_tanh"
 # exp_name1="gnerf_fvr_baseline_no_perturb64_updowndecoder1_tanh_perturb000001"
-declare -a StringArray=("hanerf_encoder" )
+declare -a StringArray=( "hanerf_encoder_random_down4")
 gpu1=7
 
 for val in ${StringArray[@]}; do
@@ -22,8 +22,6 @@ for val in ${StringArray[@]}; do
   dataset_name1='phototourism'
   save_dir1=/mnt/cephfs/dataset/NVS/nerfInWild/experimental_results
 
-  decoder_num_res_blocks=1
-  decoder='esrgan'
   # CUDA_VISIBLE_DEVICES=7,6 python train_mask_grid_sample.py   --root_dir $root_dir1 --dataset_name $dataset_name1   --save_dir $save_dir1 --img_downscale 2 --use_cache   --N_importance 64 --N_samples 64   --num_epochs 20 --batch_size 1024   --optimizer adam --lr 5e-4 --lr_scheduler cosine   --exp_name $exp_name1 --N_emb_xyz 15 --N_vocab 1500   --maskrs_max 5e-2 --maskrs_min 6e-3 --maskrs_k 1e-3 --maskrd 0   --N_a 48 --weightKL 1e-5 --weightRecA 1e-3 --weightMS 1e-6   --num_gpus 2 --chunk 131072  --proj_name gnerf --nerf_out_dim $nerf_out_dim1 --model_mode $model_mode1 --pertubeCord
   cd /mnt/cephfs/dataset/NVS/nerfInWild/experimental_results/logs/$exp_name1/codes
   # CUDA_VISIBLE_DEVICES=$gpu1 python eval.py \
@@ -40,6 +38,6 @@ for val in ${StringArray[@]}; do
     --root_dir $root_dir1 \
     --save_dir $save_dir1 \
     --dataset_name $dataset_name1 --scene_name $exp_name1 \
-    --split test_test --img_downscale 2 \
+    --split test_test --img_downscale 4 \
     --img_wh 320 240
 done
